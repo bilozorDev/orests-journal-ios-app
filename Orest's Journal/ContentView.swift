7e3897c35e2a42ecefe4b9d9733ef3bf0e6eeec2
Alex Bilozor <bilozor.dev@gmail.com>
2025-10-04 10:04:58 -0700

    Initial Commit

---
+++ Orest's Journal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Orest's Journal
+//
+//  Created by Alex on 10/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Orest's Journal/Orest_s_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Orest_s_JournalApp.swift
+//  Orest's Journal
+//
+//  Created by Alex on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Orest_s_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
